c6db2234561afcca986e315c5f42eb47918b5ecc
SandhuJai <sandhujaikirat98@gmail.com>
2018-06-10 07:03:08 +0530

    Add users class and updateUserList

--- server/server.js
+++ server/server.js
@@ -6,19 +6,36 @@ const SocketIO = require('socket.io');
 const publicPath = path.join(__dirname + '/../public');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 let app = express();
 let server = http.createServer(app);
 let io = SocketIO(server);
+
+let users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat Room'));
+    socket.on('join', (params, callback) => {
+        if(!isRealString(params.name) || !isRealString(params.room)) {
+            return callback('Name and Room Name are required');
+        }
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+        socket.join(params.room);
+        // socket.leave('Developers');
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat Room'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} just joined`));
+
+        callback();
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
@@ -31,7 +48,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('Disconnected from the client');
+        let user = users.removeUser(socket.id);
+        if(user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the chat room`));
+        }
     });
 });
 
